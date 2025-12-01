7dc729c3a554380621395237436632d28eb84a3f
Christopher Johnston <chris@fuzzylizard.com>
2025-12-01 16:32:58 -0500

    adds user posts on user profile page, caches updated with new post

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,13 +6,24 @@ export function getUserDataSelect(loggedInUserId: string) {
     username: true,
     displayName: true,
     avatarUrl: true,
+    bio: true,
+    createdAt: true,
     followers: {
       where: { followerId: loggedInUserId },
       select: { followerId: true },
     },
-    _count: { select: { followers: true } },
+    _count: {
+      select: {
+        posts: true,
+        followers: true,
+      },
+    },
   } satisfies Prisma.UserSelect;
 }
+
+export type UserData = Prisma.UserGetPayload<{
+  select: ReturnType<typeof getUserDataSelect>;
+}>;
 
 export function getPostDataInclude(loggedInUserId: string) {
   return {
